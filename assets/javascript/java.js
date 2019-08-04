4cb0614fbb9d1efb9061f58235ee74b25bce3c5c
Jesse Denick <jessedenick@gmail.com>
2019-08-04 00:45:10 -0400

    added buttons and was able to push them successfully to array

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,1 +1,28 @@
-alert("Reset");
+alert("Resetting..")
+
+var gifanimals = ["Lion", "Bear", "Dog", "Panda"];
+
+function renderButtons() {
+
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < gifanimals.length; i++) {
+
+        var a = $("<button>");
+        a.addClass("gif");
+        a.attr("data-name", gifanimals[i]);
+        a.text(gifanimals[i]);
+        $("#buttons-view").append(a);
+    }
+}
+
+$("#add-gif").on("click", function(event){
+    event.preventDefault();
+
+    var gif = $("#gif-input").val().trim();
+    gifanimals.push(gif);
+
+    renderButtons();
+});
+
+    renderButtons();
